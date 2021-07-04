57117b6b52fbb6de6f766d8d5fe8a51d897d2471
Min <minmusxminmus@hotmail.com>
2021-07-04 16:27:51 +0200

    Compatibility for companion objects

--- src/main/kotlin/other/Logging.kt
+++ src/main/kotlin/other/Logging.kt
@@ -18,7 +18,12 @@ package other
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import kotlin.reflect.full.companionObject
 
 interface Logging
 
-inline fun <reified T : Logging> T.logger(): Logger = LoggerFactory.getLogger(T::class.java)
+inline fun <reified T : Logging> T.logger(): Logger = LoggerFactory.getLogger(getClassForLogging(T::class.java))
+
+fun <T : Any> getClassForLogging(javaClass: Class<T>): Class<*> = javaClass.enclosingClass?.takeIf {
+    it.kotlin.companionObject?.java == javaClass
+} ?: javaClass
