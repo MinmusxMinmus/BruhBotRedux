99ca2da59b25b3576f4137e6903e2808e5e69a60
Min <minmusxminmus@hotmail.com>
2021-07-25 14:25:20 +0200

    feat: Logging revert
    It didn't work

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,11 @@ repositories {
 dependencies {
     implementation(fileTree("lib"))
     implementation(kotlin("stdlib"))
+    implementation(kotlin("reflect"))
 
     implementation(group = "net.dv8tion", name = "JDA", version = "4.3.0_277")
+    implementation(group = "org.apache.logging.log4j", name = "log4j-slf4j-impl", version = "2.9.0")
+    implementation(group = "com.fasterxml.jackson.dataformat", name = "jackson-dataformat-yaml", version = "2.7.3")
 }
 
 tasks.withType<KotlinCompile> {
